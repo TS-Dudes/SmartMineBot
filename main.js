7f260a67b43aef3f33280a0035c2d2ede10b5a9f
poyarik <49971285+poyarik@users.noreply.github.com>
2020-11-27 12:46:56 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const mineflayer = require('mineflayer')
-const pathfinder = require('mineflayer-pathfinder').pathfinder
-const collectBlock = require('mineflayer-collectblock').plugin
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const GoalFollow = goals.GoalFollow
+const GoalBlock = goals.GoalBlock
+const autoeat = require('mineflayer-auto-eat')
 
 const bot = mineflayer.createBot({
     host: 'SERVER IP HERE',
