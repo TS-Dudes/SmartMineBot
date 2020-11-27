2e0e8ee3d782513ef0e65747a653b1c414eb8880
poyarik <49971285+poyarik@users.noreply.github.com>
2020-11-27 12:52:11 +0600

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ const bot = mineflayer.createBot({
     username: 'TestBot'
 })
 
+bot.loadPlugin(pathfinder)
+bot.loadPlugin(autoeat)
+
+bot.once('spawn', () => {
+		bot.chat("Всем привет пацаны! Я ИИ по имени Гоша")
+		bot.autoEat.options = {
+			priority: 'foodPoints',
+			startAt: 10,
+			bannedFood: []
+		  }
+
+		bot.setQuickBarSlot(0)
+        const mobb = bot.players['Poyarik']
+        const mob = mobb.entity
+
+		if (!mob) {
+			return
+		}
+
+		const mcData = require('minecraft-data')(bot.version)
+		const movements = new Movements(bot, mcData)
+		movements.scafoldingBlocks = []
+
+		bot.pathfinder.setMovements(movements)
+
+		const goal = new GoalFollow(mob, 1)
+		bot.pathfinder.setGoal(goal, true)
