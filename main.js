4494f0e97b2c41c6234b7f17f8806c1e0b679676
poyarik <49971285+poyarik@users.noreply.github.com>
2020-11-27 12:45:35 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const mineflayer = require('mineflayer')
 const pathfinder = require('mineflayer-pathfinder').pathfinder
 const collectBlock = require('mineflayer-collectblock').plugin
+
+const bot = mineflayer.createBot({
+    host: 'SERVER IP HERE',
+    port: SERVER PORT HERE,
+    username: 'TestBot'
+})
+
